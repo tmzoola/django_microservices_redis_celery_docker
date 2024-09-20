5e2452fd3445c4a37579ea706985d8a85d2343e0
Murodjon Tokhirov <murodtokh1997@gmail.com>
2024-09-20 10:21:53 +0500

    Microservices day 3-1 (made 2 subdomains from windows settings and separated backend project by  domain)

--- mailhub/mailhub/settings.py
+++ mailhub/mailhub/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+f9h9ybw$evmu7&#(w+c_p#!zykm0o+d5pdkim*g!uzkrwj_dd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -116,8 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
+STATIC_URL = '/static/'
+STATIC_ROOT = '/app/staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
--- users/users/settings.py
+++ users/users/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 import environ
-from celery.schedules import crontab
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,7 +28,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG', default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -130,8 +129,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
+STATIC_URL = '/static/'
+STATIC_ROOT = '/app/staticfiles'
+
+
+LOGIN_URL = '/users/admin/login/'
+LOGIN_REDIRECT_URL = '/users/admin/'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
